327259498003b3e900f9f08f907f954b13b93850
luckys-lnz <lalucky22@gmail.com>
2025-03-30 17:57:02 +0000

    helper function for redis

---
+++ web-scraper/redis_helper.h
@@ -0,0 +1,18 @@
+#ifndef REDIS_HELPER_H
+#define REDIS_HELPER_H
+
+#include <hiredis/hiredis.h>
+
+// Redis connection
+void init_redis();
+void close_redis();
+
+// URL deduplication
+int is_visited(const char *url);
+void mark_visited(const char *url);
+
+// URL queue management
+char *fetch_url_from_queue();
+void push_url_to_queue(const char *url);
+
+#endif
