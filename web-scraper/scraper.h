521e1ab0e58b4ff5d2b6949838d3b87ac9c79cbd
luckys-lnz <lalucky22@gmail.com>
2025-03-31 22:18:32 +0000

    updated extract_hrefs()

--- web-scraper/scraper.h
+++ web-scraper/scraper.h
@@ -21,7 +21,7 @@ struct Memory {
 size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp);
 void extract_title(const char *html);
 void extract_meta(const char *html);
-void extract_hrefs(const char *html);
+void extract_hrefs(const char *html, const char *base_url);
 void fetch_url(const char *url, struct Memory *chunk);
 
 #endif // SCRAPER_H
