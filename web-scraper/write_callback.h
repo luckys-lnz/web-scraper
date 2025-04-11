c68ee7f9a587c2e4649ce407c8cceb889bbc7c73
luckys-lnz <lalucky22@gmail.com>
2025-04-11 23:25:45 +0000

    feat: Update write_callback function documentation for clarity

--- web-scraper/write_callback.h
+++ web-scraper/write_callback.h
@@ -2,8 +2,11 @@
 #define WRITE_CALLBACK_H
 
 #include "scraper.h"
+#include <stddef.h>
 
-// Function declaration for write_callback
+/**
+ * Callback function for CURL to write received data.
+ */
 size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp);
 
 #endif // WRITE_CALLBACK_H
