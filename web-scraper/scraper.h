665d9ceb7c50e3e7a5e05424867ff674c1f25f09
luckys-lnz <lalucky22@gmail.com>
2025-04-11 23:26:24 +0000

    feat: Add URL processing task data structure and function prototypes to scraper header

--- web-scraper/scraper.h
+++ web-scraper/scraper.h
@@ -10,6 +10,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <pthread.h>
+#include "thread_pool.h"
+#include "stats.h"  // Include stats.h for statistics types
 
 // Struct for storing fetched HTML data
 struct Memory {
@@ -17,10 +21,21 @@ struct Memory {
   size_t size;
 };
 
+// URL processing task data
+typedef struct {
+    char *url;
+    int priority;
+} url_task_t;
+
 // Function prototypes
 void extract_title(const char *html);
 void extract_meta(const char *html);
 void extract_hrefs(const char *html, const char *base_url);
 int is_allowed_by_robots(const char *url);
+
+// URL processing functions
+void *process_url(void *arg);
+void init_scraper_pool(int thread_count);
+void cleanup_scraper_pool();
 
 #endif // SCRAPER_H
